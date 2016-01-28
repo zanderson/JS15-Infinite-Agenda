aba0fb58a743e2779789442b418c0ee7e20fd952
Student <student@RU-012.local>
2016-01-28 16:32:35 -0700

    update

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-angular.module('calendar', [])
-   .controller('agendacontroller', ['$scope', function($scope){
-   		var dates = {};
-       // $scope.showPopup = false;
-       // $scope.click = function(show) {
-       //     $scope.showPopup = show;    
-       //     }    
-    	$scope.date = new Date();
-
-   }]);
+angular.module('agenda', [])
+            .controller('thectl', ['$scope', function($scope){
+                    $scope.agendaItems = [];
+                    for (var i = 0; i < 10; i++) {
+                        var date = new Date();
+                        date.setDate(date.getDate() + i);
+                        $scope.agendaItems.push({
+                            date: date,
+                            text: ''
+                        });
+                    }
+                }
+            ]);
 
